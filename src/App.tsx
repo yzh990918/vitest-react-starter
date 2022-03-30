cc24dcc3f22d7ba47fefd70032f55507a0ceaea3
磨痕 <251205668@qq.com>
2022-03-30 18:44:04 +0800

    feat: change hash route

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import { Route, HashRouter as Router, Routes } from 'react-router-dom'
 import Footer from './components/Footer'
 import Home from '@/pages/home'
 import Hi from '@/pages/hi'
